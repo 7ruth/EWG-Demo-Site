466411477492132c1346a0e24c7cf6266d719cc8
Martin Terskin <martinas.terskin@gmail.com>
2016-07-02 14:11:22 -0400

    final to gh-pages

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ if (mobile) {
   $('#group1 h1').css('font-size', '1.5em');
   $('#group1 h2').css('font-size', '1em');
   $('#group1 h3').css('font-size', '.75em');
+  $('#group1').css('height', '550px');
   $('#group2').css('height', '450px');
   $('#group3').css('height', '400px');
   $('#group4').css('height', '400px');
-  $('#group5').css('height', '400px');
-  $('#group6').css('height', '450px');
+  $('#group5').css('height', '450px');
+  $('#group6').css('height', '550px');
   $('#group7').css('height', '400px');
-  $('#group7 img').css('left', '25%');
-  $('#group7 img').css('transform', '-25%');
+  $('#group7 img').css('left', '22.5%');
+  $('#group7 img').css('transform', '-22.5%');
     //adjust font sizes
     //adjust logo positioning
     //size of each layer group
