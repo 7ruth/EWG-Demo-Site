e8d4931bbf51772b9db886d658992762626fba69
Martin Terskin <martinas.terskin@gmail.com>
2016-07-02 13:48:20 -0400

    final to gh-pages

---
+++ script.js
@@ -0,0 +1,16 @@
+var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+// All elements to be transparent until full load
+$('body').css('opacity', 0);
+$(window).load(function() {
+  $('body').css('opacity', 1);
+});
+//Mobile specific setting
+if (mobile) {
+    //adjust font sizes
+    //adjust logo positioning
+    //size of each layer group
+  } else {
+    //adjust font sizes
+    //adjust logo positioning
+    //size of each layer group
+  }
